f9f6858761fee7a7cba150a6430bf005bdfdc720
Sam Laderoute <laderoute.samuel@gmail.com>
2021-08-17 13:23:31 -0400

    paths are now relative (and improved visuals and added comments)

--- Scripts/col_pick.py
+++ Scripts/col_pick.py
@@ -6,9 +6,11 @@ import cv2
 import pygame
 from PIL import ImageGrab
 from functools import partial
+
 ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 
-from ctypes import windll  #windows only
+from ctypes import windll  # windows only
+
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 SetWindowPos = windll.user32.SetWindowPos
 
@@ -21,7 +23,7 @@ def rgb2hex(rgb):
 
 
 def copy2clip(txt):
-    cmd = 'echo '+txt.strip()+'|clip'
+    cmd = 'echo ' + txt.strip() + '|clip'
     return subprocess.check_call(cmd, shell=True)
 
 
@@ -29,55 +31,91 @@ class MyException(Exception):
     pass
 
 
+def palette_swap(surf, old_c, new_c):
+    img_copy = pygame.Surface(surf.get_size())
+    img_copy.fill(new_c)
+    surf.set_colorkey(old_c)
+    img_copy.blit(surf, (0, 0))
+    return img_copy
+
+
 def on_activate_col_pick():
-    print('<cmd>+<shift>+c pressed')
+    print('<alt>+c pressed (COLOR PICK)')
     # Take A ScreenShot & Save to File
     myscreenshot = pyautogui.screenshot()
-    myscreenshot.save(r'D:/_Files/Programming/Python/BG_utils/Assets/screenshot.png')
+    myscreenshot.save(r'../Assets/screenshot.png')
 
-    # Display screenshot
+    # Start pygame instance
     pygame.init()
     pygame.font.init()
-    myfont = pygame.font.SysFont('Consolas', 30)
+    font = pygame.font.SysFont('Consolas', 30)
     pygame.mouse.set_visible(False)
-    img = cv2.imread('D:/_Files/Programming/Python/BG_utils/Assets/screenshot.png')
+    # pygame.mouse.set_cursor(pygame.cursors.broken_x)
+    img = cv2.imread('../Assets/screenshot.png')
     width, height = img.shape[1], img.shape[0]
+    # create main canvas
     screen = pygame.display.set_mode([width, height])
-    # force on top
-    # SetWindowPos(pygame.display.get_wm_info()["window"], 0, 0, 0, 0, 0, 2 | 1)
-    pyimg = pygame.image.load('D:/_Files/Programming/Python/BG_utils/Assets/screenshot.png').convert()
 
-    r = 80
-    zoom = 3
-    cropped_mask = pygame.Surface((r * zoom*2, r * zoom*2))
+    # load img and create dark version
+    pyimg = pygame.image.load('../Assets/screenshot.png').convert()
+    dark = pygame.Surface(screen.get_size())
+    dark.fill((0, 0, 0))
+    dark.set_alpha(100)
+    pyimg_dark = pyimg.copy()
+    pyimg_dark.blit(dark, (0, 0))
+
+    # radius of search circle and zoom level
+    r = 100
+    zoom = 4
+    # create circular mask
+    cropped_mask = pygame.Surface((r * zoom * 2, r * zoom * 2))
     cropped_mask.fill((0, 0, 0))
-    pygame.draw.circle(cropped_mask, (255, 255, 255), (r*zoom, r*zoom), r)
+    pygame.draw.circle(cropped_mask, (255, 255, 255), (r * zoom, r * zoom), r)
 
     PICK_COLOR = True
     while PICK_COLOR:
-        # draw BG
-        screen.blit(pyimg, (0, 0))
-
-        # draw crosshair
+        # draw BG (display screenshot)
+        screen.blit(pyimg_dark, (0, 0))
+        # get mouse_x mouse_y
         x, y = pygame.mouse.get_pos()
-        cropped = pygame.Surface((r*2, r*2))
-        cropped.blit(screen, (0, 0), (x-r, y-r, r*zoom, r*zoom))
-        cropped = pygame.transform.scale(cropped, (r*zoom*2, r*zoom*2))
-        cropped.blit(cropped_mask, (0, 0), special_flags=pygame.BLEND_MULT)
-        cropped.set_colorkey((0, 0, 0))
-
-        screen.blit(cropped, (x-r*zoom, y-r*zoom))
-        pygame.draw.circle(screen, (255, 255, 255), (x, y), r, 1)
-        pygame.draw.line(screen, (255, 255, 255), (x, y+r), (x, y-r))
-        pygame.draw.line(screen, (255, 255, 255), (x+r, y), (x-r, y))
+        # get color at mouse_x mouse_y
         color = pygame.Color(pygame.PixelArray(pyimg)[x, y])
         alpha, red, g, b = color
         rgb = [red, g, b]
         hex_val = (rgb2hex(rgb))
-        pygame.draw.rect(screen, (red, g, b), (round(x-r/4), round(y+r/4), round(r/2), round(r/2)), border_radius=round(r/8))
-        text = myfont.render(hex_val, False, (255, 255, 255))
-        screen.blit(text, (x-round(text.get_width()/2), y+r))
 
+        # Create zoomed portion (copy of BG) #
+        cropped = pygame.Surface((r * 2, r * 2))
+        cropped.blit(pyimg, (0, 0), (x - r, y - r, r * zoom, r * zoom))
+        # swap true black to something close so it isn't color_keyed out
+        cropped = palette_swap(cropped, (0, 0, 0), (2, 2, 2))
+        # zoom in and mask out circle
+        cropped = pygame.transform.scale(cropped, (r * zoom * 2, r * zoom * 2))
+        cropped.blit(cropped_mask, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+        # make all black pix transparent
+        cropped.set_colorkey((0, 0, 0))
+        # blit onto surface
+        screen.blit(cropped, (x - r * zoom, y - r * zoom))
+
+        # draw cross-hair
+        crosshair = pygame.Surface((r*2, r*2))
+        crosshair.fill((0, 0, 0))
+        pygame.draw.circle(crosshair, (255, 255, 255), (r, r), r, 1)
+        pygame.draw.line(crosshair, (255, 255, 255), (r, 0), (r, 2*r))
+        pygame.draw.line(crosshair, (255, 255, 255), (0, r), (2*r, r))
+        # draw color preview box
+        pygame.draw.rect(crosshair, (255, 255, 255),
+                         (round(r - r / 4) - 1, round(r + r / 4) - 1, round(r / 2) + 2, round(r / 2) + 2),
+                         border_radius=round(r / 8))
+        crosshair.set_colorkey((0, 0, 0))
+        screen.blit(crosshair, (x-r, y-r))
+        pygame.draw.rect(screen, rgb, (round(x - r / 4), round(y + r / 4), round(r / 2), round(r / 2)),
+                         border_radius=round(r / 8))
+        # draw hex text
+        text = font.render(hex_val, False, (255, 255, 255))
+        screen.blit(text, (x - round(text.get_width() / 2), y + r))
+
+        # update display
         pygame.display.update()
 
         for event in pygame.event.get():
@@ -90,12 +128,12 @@ def on_activate_col_pick():
 
 
 def on_activate_kill():
-    print('<ctrl>+<shift>+<esc> pressed')
+    print('<esc> pressed (QUIT)')
     pygame.quit()
     raise MyException("kill")
 
 
 with keyboard.GlobalHotKeys({
-    '<cmd>+<shift>+c': on_activate_col_pick,
-    '<ctrl>+<shift>+<esc>': on_activate_kill}) as h:
+    '<alt>+c': on_activate_col_pick,
+    '<esc>': on_activate_kill}) as h:
     h.join()
